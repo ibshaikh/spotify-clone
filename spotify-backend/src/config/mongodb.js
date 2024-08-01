079003f631db4c60cda66e202ca170cd5ac09036
Ibram <shaikhibuu07@gmail.com>
2024-08-01 20:19:30 +0530

    first commit

---
+++ spotify-backend/src/config/mongodb.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+
+    mongoose.connection.on('connected', () => {
+        console.log("✔ Connection Established")
+    })
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/spotify`);
+
+}
+
+export default connectDB;

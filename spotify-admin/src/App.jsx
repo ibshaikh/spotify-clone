0661a0492f6952d6ac14c4905be33f8365ed8120
Ibram Shaikh <123507618+ibshaikh@users.noreply.github.com>
2024-08-02 12:35:46 +0530

    Update App.jsx

--- spotify-admin/src/App.jsx
+++ spotify-admin/src/App.jsx
@@ -10,7 +10,7 @@ import ListAlbum from './pages/ListAlbum'
 import SideBar from './components/SideBar';
 import Navbar from './components/Navbar';
 
-export const url = ' http://localhost:4000'
+export const url = 'https://spotify-clone-bi0n.onrender.com'
 
 
 const App = () => {
